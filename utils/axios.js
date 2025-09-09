40f72e9e479d00587ffec87052bacce4e1817485
md-fahad1 <20-43328-1@student.aiub.edu>
2025-09-09 23:58:40 +0600

    error fix

--- utils/axios.js
+++ utils/axios.js
@@ -1,26 +1,26 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
-//   withCredentials: true,
-// });
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  withCredentials: true,
+});
 
-// api.interceptors.response.use(
-//   (res) => res,
-//   async (err) => {
-//     console.log("hello interceptors");
-//     const originalRequest = err.config;
-//     if (err.response?.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-//       try {
-//         await api.post("/auth/refresh", {}, { withCredentials: true });
-//         return api(originalRequest);
-//       } catch (refreshErr) {
-//         if (typeof window !== "undefined") window.location.href = "/login";
-//       }
-//     }
-//     return Promise.reject(err);
-//   }
-// );
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    console.log("hello interceptors");
+    const originalRequest = err.config;
+    if (err.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await api.post("/auth/refresh", {}, { withCredentials: true });
+        return api(originalRequest);
+      } catch (refreshErr) {
+        if (typeof window !== "undefined") window.location.href = "/login";
+      }
+    }
+    return Promise.reject(err);
+  }
+);
 
-// export default api;
+export default api;
